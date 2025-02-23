ec982b6c1897621f380f10ecb933e41910302a33
0HugoHu <0@hugohu.top>
2025-02-22 18:08:42 -0800

    Initial Commit

---
+++ HLogger/HLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HLoggerApp.swift
+//  HLogger
+//
+//  Created by Hugooooo on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct HLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
